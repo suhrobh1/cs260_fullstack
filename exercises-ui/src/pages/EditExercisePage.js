3f6b8b523e2b0ee833c910c72fd6ac0c2457a61a
Suhrob Hasanov <suhrobhasanov1@gmail.com>
2023-03-06 23:21:01 -0800

    UPDATE PAGE

--- exercises-ui/src/pages/EditExercisePage.js
+++ exercises-ui/src/pages/EditExercisePage.js
@@ -3,10 +3,69 @@ import { useNavigate } from 'react-router-dom';
 
 export const EditExercisePage = () => {
 
+    const [exercise, setExercise] = useState({
+        name: "",
+        reps: "",
+        weight: "",
+        unit: "",
+        date: "",
+      });
+
+    // exercise inputs are being captured and stored
+    const handleChange = (e) => {
+        setExercise({...exercise, [e.target.name]: e.target.value});
+      };
+    
+    const loadExercises = async () => {
+        const response = await fetch('/exercises');
+        const data = await response.json();
+        setExercise(data)
+    }
+
+
+
     return (
         <div>
-            <h1>Edit Exercise</h1>
-        </div>
+        <h1>Add Exercise</h1>
+        <input
+            type="text"
+            placeholder="Enter name here"
+            name = "name"
+            value={exercise.name}
+            onChange={(e) => handleChange(e)}/>
+        <input
+            type="number"
+            value={exercise.reps}
+            placeholder="Enter reps here"
+            name='reps'
+            onChange={(e) => handleChange(e)} />
+        <input
+            type="number"
+            placeholder="Enter weight here"
+            name='weight'
+            value={exercise.weight}
+            onChange={(e) => handleChange(e)}/>
+         <select
+          value={exercise.unit}
+          onChange={(e) => handleChange(e)}
+          name="unit"
+        >
+          <option value="none" defaultValue hidden>
+            units
+          </option>
+          <option value="kg">kg</option>
+          <option value="lbs">lbs</option>
+        </select>
+         <input
+            type="date"
+            placeholder="Enter date here"
+            name='date'
+            value={exercise.date}
+            onChange={(e) => handleChange(e)} />
+        <button
+            onClick={addExercise}
+        >Add</button>
+    </div>
     );
 }
 
